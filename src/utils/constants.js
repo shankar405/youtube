285cdadf49893ebfd249079e6ee7f9ff24503068
shankar405 <86355570+shankar405@users.noreply.github.com>
2025-10-27 08:20:11 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 
-export const GOOGLE_API_KEY=""
+
+export const GOOGLE_API_KEY="AIzaSyAVjYnmzIyZYAjIfAfgLBlEJ_PfLd5vz2Y"
 
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+
